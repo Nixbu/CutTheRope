b00b54f0a668e4aa789f45ef582fc1a5aed6ffe0
Nixbu <83171543+Nixbu@users.noreply.github.com>
2024-06-23 14:04:54 +0300

    Reading level status from file

--- src/LevelSelectState.cpp
+++ src/LevelSelectState.cpp
@@ -53,7 +53,7 @@ void LevelSelectState::addButtons(Controller &controller)
 	
 
 	std::ifstream levelPlaylist(m_levelPlaylist);
-	std::string line;
+	std::string line, levelFileName, status;
 	int levelNum = 1;
 	auto position = FIRST_BUTTON_POSITION;
 	const int buttonsPerRow = ROW_NUM_OF_LEVELS;  // Number of buttons per row
@@ -61,26 +61,16 @@ void LevelSelectState::addButtons(Controller &controller)
 
 	while (std::getline(levelPlaylist, line))
 	{
-		if (levelNum == 1)
-		{
-			// Add the level button
-			this->m_levelButtons.addButton(std::make_unique<Button>(
-				sf::Vector2f(position),
-				manager.getImage("0Stars"),
-				MENU_BUTTON_DEFA_SIZE,
-				std::make_unique<PlayLevel>(controller, controller.getPlayingState(), line, levelNum, open)
-			));
-		}
-		else
-		{
-			// Add the level button
-			this->m_levelButtons.addButton(std::make_unique<Button>(
-				sf::Vector2f(position),
-				manager.getImage("LockedLevel"),
-				MENU_BUTTON_DEFA_SIZE,
-				std::make_unique<PlayLevel>(controller, controller.getPlayingState(), line, levelNum, open)
-			));
-		}
+		std::istringstream iss(line);
+
+		iss >> levelFileName >> status;
+		// Add the level button
+		this->m_levelButtons.addButton(std::make_unique<Button>(
+			sf::Vector2f(position),
+			manager.getImage(status),
+			MENU_BUTTON_DEFA_SIZE,
+			std::make_unique<PlayLevel>(controller, controller.getPlayingState(), levelFileName, levelNum, open)
+		));
 
 		// Update the position for the next button
 		levelNum++;
@@ -92,9 +82,6 @@ void LevelSelectState::addButtons(Controller &controller)
 			position.y += LEVEL_BUTTON_SHIFT_Y;   // Move down to the next row
 		}
 	}
-
-
-	
 }
 
 void LevelSelectState::addLevelButtons()
